8fed12bca70f103199375bfc8f64ba856274c7a6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-29 18:14:56 +0000

    Deploy monroews/EnvEngLabTextbook to github.com/monroews/EnvEngLabTextbook.git:test

--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,11 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v.0.0.44',
+    VERSION: '',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    SOURCELINK_SUFFIX: '.txt'
 };
